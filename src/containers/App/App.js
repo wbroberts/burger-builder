65aa58a7572d1d297ee9075faf7644ad890b3298
wbroberts <willbarronroberts@gmail.com>
2018-07-19 10:35:55 -0600

    init

---
+++ src/containers/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Layout from '../../components/Layout/Layout';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Burger Builder</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
